0d70c093a88f0c68aa0b417bd844e259cff21532
chathurperera <chathuraperera007@gmail.com>
2024-02-22 21:21:23 +0530

    chore: project setup

---
+++ project2/project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by chathura on 2024-02-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
